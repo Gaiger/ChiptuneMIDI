adfdfbd3d86abdc3fca5bb2fa29911b1efcaf26d
垓恪Gaiger@i5-14500 <drsteingaiger@gmail.com>
2026-02-21 23:57:40 +0800

    refactor(qt): lazily create MelodicTimbreFrame only for melodic channels
    - Avoid instantiating MelodicTimbreFrame for percussion ChannelNodeWidget instances:
      - Initialize m_p_melodic_timbre_frame to nullptr in the initializer list.
      - Create (new) MelodicTimbreFrame only when channel_index is not MIDI_PERCUSSION_CHANNEL.
    - Keep percussion nodes collapsed as before; melodic nodes continue to expose timbre UI.
    
    No UI or behavior changes intended; this change only reduces unnecessary widget creation.

--- qt/ChannelNodeWidget.cpp
+++ qt/ChannelNodeWidget.cpp
@@ -15,7 +15,7 @@
 ChannelNodeWidget::ChannelNodeWidget(int channel_index, int instrument_index, QWidget *parent) :
 	QWidget(parent),
 	m_channel_index(channel_index),
-	m_p_melodic_timbre_frame(new MelodicTimbreFrame(this)),
+	m_p_melodic_timbre_frame(nullptr),
 	ui(new Ui::ChannelNodeWidget)
 {
 	ui->setupUi(this);
@@ -34,39 +34,45 @@ ChannelNodeWidget::ChannelNodeWidget(int channel_index, int instrument_index, QW
 	m_expanded_size = QWidget::size();
 	m_collapsed_size = QSize(m_expanded_size.width(), m_expanded_size.height() - ui->MelodicTimbreWidget->height());
 	QWidget::setFixedSize(m_collapsed_size);
-	QString instrument_name;
-#define CHIPTUNE_INSTRUMENT_NOT_SPECIFIED			(-1)
-#define CHIPTUNE_INSTRUMENT_UNUSED_CHANNEL			(-2)
+
+	QString instrument_name = QString("Unknown");
 	do
 	{
-		if(CHIPTUNE_INSTRUMENT_NOT_SPECIFIED == instrument_index){
-			instrument_name = QString("Not Specified");
-			break;
-		}
-		if(CHIPTUNE_INSTRUMENT_UNUSED_CHANNEL == instrument_index){
-			instrument_name = QString("Unused channel");
+		if(MIDI_PERCUSSION_CHANNEL == channel_index){
+			instrument_name = QString("Percussion");
+			ui->ExpandCollapsePushButton->setEnabled(false);
 			break;
 		}
 
-		instrument_name = GetInstrumentNameString(instrument_index);
-	}while(0);
-	if(MIDI_PERCUSSION_CHANNEL == channel_index){
-		instrument_name = QString("Percussion");
-	}
+#define CHIPTUNE_INSTRUMENT_NOT_SPECIFIED			(-1)
+#define CHIPTUNE_INSTRUMENT_UNUSED_CHANNEL			(-2)
+		do
+		{
+			if(CHIPTUNE_INSTRUMENT_NOT_SPECIFIED == instrument_index){
+				instrument_name = QString("Not Specified");
+				break;
+			}
+			if(CHIPTUNE_INSTRUMENT_UNUSED_CHANNEL == instrument_index){
+				instrument_name = QString("Unused channel");
+				break;
+			}
+
+			instrument_name = GetInstrumentNameString(instrument_index);
+		}while(0);
+
+		m_p_melodic_timbre_frame = new MelodicTimbreFrame(this);
+		QGridLayout *p_layout = new QGridLayout(ui->MelodicTimbreWidget);
+		p_layout->addWidget(m_p_melodic_timbre_frame, 0, 0);
+		p_layout->setContentsMargins(0, 0, 0, 0);
+		p_layout->setSpacing(0);
+
+		QObject::connect(m_p_melodic_timbre_frame, &MelodicTimbreFrame::TimbreChanged, this,
+						 &ChannelNodeWidget::HandleMelodicTimbreFrameTimbreChanged);
+	} while(0);
+
 	QString string = "#"+ QString::number(channel_index) +" " + instrument_name;
 	ui->ExpandCollapsePushButton->setStyleSheet("text-align:left;");
 	ui->ExpandCollapsePushButton->setText(string);
-
-	QGridLayout *p_layout = new QGridLayout(ui->MelodicTimbreWidget);
-	p_layout->addWidget(m_p_melodic_timbre_frame, 0, 0);
-	p_layout->setContentsMargins(0, 0, 0, 0);
-	p_layout->setSpacing(0);
-
-	QObject::connect(m_p_melodic_timbre_frame, &MelodicTimbreFrame::TimbreChanged, this,
-					 &ChannelNodeWidget::HandleMelodicTimbreFrameTimbreChanged);
-	if(MIDI_PERCUSSION_CHANNEL == channel_index){
-		ui->ExpandCollapsePushButton->setEnabled(false);
-	}
 }
 
 /**********************************************************************************/
